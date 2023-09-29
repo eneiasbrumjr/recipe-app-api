84fb17380d84b73f85ddb75c4d01308cfc6a2ae0
Eneias S Brum Jr <eneiassbrumjr@gmail.com>
2023-09-28 23:05:18 -0300

    Added db configurations, and checks to wait for db

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add (x, y):
+    """Add x and y and return result."""
+    return x + y
+
+def subtract (x, y):
+    """Subtract y from x and return result."""
+    return x - y
