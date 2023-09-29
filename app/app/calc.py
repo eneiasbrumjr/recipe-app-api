e82e983fa31e7346d119f3560565d7a712480803
Eneias S Brum Jr <eneiassbrumjr@gmail.com>
2023-09-28 23:18:24 -0300

    Fix some lint requirements

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,12 @@
 Calculator functions
 """
 
-def add (x, y):
+
+def add(x, y):
     """Add x and y and return result."""
     return x + y
 
-def subtract (x, y):
+
+def subtract(x, y):
     """Subtract y from x and return result."""
     return x - y
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -9,6 +9,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test custom management commands."""
@@ -22,11 +23,11 @@ class CommandTests(SimpleTestCase):
         patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
-    def test_wait_for_db_delay(self, patched_sleep ,patched_check):
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
         patched_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
-        
+
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
